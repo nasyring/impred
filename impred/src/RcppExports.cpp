48b69232bf126ee97a0ce63fe215844fdfdbab39
nasyring <nasyrin@gmail.com>
2021-05-25 12:53:09 -0500

    Update RcppExports.cpp

--- impred/src/RcppExports.cpp
+++ impred/src/RcppExports.cpp
@@ -7,16 +7,16 @@ using namespace std;
 
 
 // randsetsMCMC
-Rcpp::List randsetsMCMC(SEXP & H, SEXP & A, SEXP & rL, SEXP & dimH, SEXP & M_samp);
+Rcpp::List randsetsMCMC(RMatrix<double> H, RMatrix<double> A, RVector<double> rL, RVector<double> dimH, RVector<double> M_samp);
 RcppExport SEXP impred_randsetsMCMC(SEXP HSEXP, SEXP SEXPA, SEXP SEXPrL, SEXP SEXPdimH, SEXP SEXPM_samp){
 BEGIN_RCPP
     Rcpp::RObject __result;
     Rcpp::RNGScope __rngScope;
-    Rcpp::traits::input_parameter< SEXP & >::type H(HSEXP);
-    Rcpp::traits::input_parameter< SEXP & >::type A(ASEXP);
-    Rcpp::traits::input_parameter< SEXP & >::type rL(rLSEXP);
-    Rcpp::traits::input_parameter< SEXP & >::type dimH(dimHSEXP);
-    Rcpp::traits::input_parameter< SEXP & >::type M_samp(M_sampSEXP);
+    Rcpp::traits::input_parameter< RMatrix<double> >::type H(HSEXP);
+    Rcpp::traits::input_parameter< RMatrix<double> >::type A(ASEXP);
+    Rcpp::traits::input_parameter< RVector<double> >::type rL(rLSEXP);
+    Rcpp::traits::input_parameter< RVector<double> >::type dimH(dimHSEXP);
+    Rcpp::traits::input_parameter< RVector<double> >::type M_samp(M_sampSEXP);
     __result = Rcpp::wrap(randsetsMCMC(H,A,rL,dimH,M_samp));
     return __result;
 END_RCPP
