0281d94c1f4abe036fa1fbfe760083f18e590793
nasyring <nasyrin@gmail.com>
2021-05-25 15:49:46 -0500

    Update impred.cpp

--- impred/src/impred.cpp
+++ impred/src/impred.cpp
@@ -13,7 +13,7 @@ using namespace std;
 
 #include <cmath>
 #include <algorithm>
-/*
+
 Rcpp::List randsetsMCMC(NumericMatrix H, NumericMatrix A, NumericVector rL, NumericVector dimH, NumericVector M_samp) {
 	
 	List result;
@@ -23,26 +23,27 @@ Rcpp::List randsetsMCMC(NumericMatrix H, NumericMatrix A, NumericVector rL, Nume
 	NumericVector propsd(1,0.0);
 	NumericVector u(2,1.0);
 	NumericVector uprop(2,0.0);
+		NumericVector uprop1(2,0.0);
 	NumericVector logjointold(1,0.0);
 	NumericVector logjointnew(1,0.0);
+		NumericVector logjointold1(1,0.0);
+	NumericVector logjointnew1(1,0.0);
 	NumericVector logjointdiff(1,0.0);
 	NumericVector uu(1,0.0);
 	NumericVector zeroes = NumericVector(H2*1, 0.0); 
         NumericMatrix U = NumericMatrix(H2, 1, zeroes.begin());
 	arma::mat Aa = as<arma::mat>(A);
 	arma::mat arg;
-		arma::mat arg1;
 	NumericVector postsamples0(M,0.0);
 	NumericVector postsamples1(M,0.0);
 	arma::mat Ua = as<arma::mat>(U);
-	arma::mat Ua1 = as<arma::mat>(U);
 	
 	for(int h = 0; h<H1; h++){
 		U(h,0) = H(h,0);	
 	}
 
 for(int j=0; j<M; j++) {
-		for(int i=0; i<1; i++){
+		for(int i=0; i<2; i++){
 			if( i==0 ){
 				propsd[0] = 10.0;	
 			}else {
@@ -52,9 +53,7 @@ for(int j=0; j<M; j++) {
 			U(H1,0) = uprop[0];
 			U(H1+1,0) = uprop[1];
 			Ua = as<arma::mat>(U);
-			Ua1 = Ua;
 			arg = Aa*Ua;
-			arg1=arg;
 			for(int h = 0; h<H2; h++){
 				logjointold[0] = logjointold[0] + 0.5*rL[h]*arg(h,0)-0.5*exp(arg(h,0));	
 			}
@@ -66,6 +65,7 @@ for(int j=0; j<M; j++) {
 			for(int h = 0; h<H2; h++){
 				logjointnew[0] = logjointnew[0] + 0.5*rL[h]*arg(h,0)-0.5*exp(arg(h,0));	
 			}
+			logjointnew1[0]=  logjointnew[0];  logjointold1[0]=  logjointold[0];
 			logjointdiff[0] = logjointnew[0] - logjointold[0];
 			logjointdiff[0] = fmin(std::exp(logjointdiff[0]), 1.0);
 			uu[0] = R::runif(0.0,1.0);
@@ -76,26 +76,37 @@ for(int j=0; j<M; j++) {
 					postsamples1[j] = uprop[1];
 				}
 				u = uprop;
+			}else {
+				if(i==0){
+					postsamples0[j] = u[0];	
+				}else {
+					postsamples1[j] = u[1];
+				}				
 			}
+			uprop1 = uprop;
+			logjointold[0] = 0.0; logjointnew[0] = 0.0;
 		}
 	}
 result = Rcpp::List::create(Rcpp::Named("samples1") = postsamples0,Rcpp::Named("samples2") = postsamples1,
 			   Rcpp::Named("logjointold") = logjointold,
 			   Rcpp::Named("logjointnew") = logjointnew,
+			   Rcpp::Named("logjointold1") = logjointold1,
+			   Rcpp::Named("logjointnew1") = logjointnew1,
 			   Rcpp::Named("U") = U,
 			   Rcpp::Named("Ua") = Ua,
 			   Rcpp::Named("arg") = arg, 
 			    Rcpp::Named("Ua1") = Ua1,
 			   Rcpp::Named("arg1") = arg1, 
-			    Rcpp::Named("uprop") = uprop);
+			    Rcpp::Named("uprop") = uprop), 
+			    Rcpp::Named("uprop1") = uprop1);
 
 	return result;
 	
 	
 }
-*/
 
 
+/*
 Rcpp::List randsetsMCMC(NumericMatrix H, NumericMatrix A, NumericVector rL, NumericVector dimH, NumericVector M_samp) {
 	
 	List result;
@@ -171,7 +182,7 @@ result = Rcpp::List::create(Rcpp::Named("samples1") = postsamples0,Rcpp::Named("
 	
 }
 
-
+*/
 
 
 
