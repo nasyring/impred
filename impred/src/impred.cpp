4c9d7245b7d653e097baa07293800ee0c168e07f
nasyring <nasyrin@gmail.com>
2021-05-26 12:57:07 -0500

    Update impred.cpp

--- impred/src/impred.cpp
+++ impred/src/impred.cpp
@@ -137,4 +137,111 @@ result = Rcpp::List::create(Rcpp::Named("randsetpred") = randsetpred);
 	
 }
 
+double zeroin(double ax, double bx, double(*f)(double x), double tol) {
+    // code here
+/*
+double zeroin(ax,bx,f,tol)		An estimate to the root	
+Xdouble ax;				Left border | of the range	
+Xdouble bx;  				Right border| the root is seeked
+Xdouble (*f)(double x);			Function under investigation	
+Xdouble tol;				Acceptable tolerance		
+	*/
+
+  double a,b,c;				/* Abscissae, descr. see above	*/
+  double fa;				/* f(a)				*/
+  double fb;				/* f(b)				*/
+  double fc;				/* f(c)				*/
+
+  a = ax;  b = bx;  fa = (*f)(a);  fb = (*f)(b);
+  c = a;   fc = fa;
+
+  for(;;)		/* Main iteration loop	*/
+  {
+    double prev_step = b-a;		/* Distance from the last but one*/
+					/* to the last approximation	*/
+    double tol_act;			/* Actual tolerance		*/
+    double p;      			/* Interpolation step is calcu- */
+    double q;      			/* lated in the form p/q; divi- */
+  					/* sion operations is delayed   */
+ 					/* until the last moment	*/
+    double new_step;      		/* Step at this iteration       */
+    double dbl_eps = std::numeric_limits<double>::epsilon();
+	  
+    if( fabs(fc) < fabs(fb) )
+    {                         		/* Swap data for b to be the 	*/
+	a = b;  b = c;  c = a;          /* best approximation		*/
+	fa=fb;  fb=fc;  fc=fa;
+    }
+    tol_act = 2*dbl_eps*fabs(b) + tol/2;
+    new_step = (c-b)/2;
+
+    if( fabs(new_step) <= tol_act || fb == (double)0 )
+    {
+      return b;				/* Acceptable approx. is found	*/
+    }
+
+    			/* Decide if the interpolation can be tried	*/
+    if( fabs(prev_step) >= tol_act	/* If prev_step was large enough*/
+	&& fabs(fa) > fabs(fb) )	/* and was in true direction,	*/
+    {					/* Interpolatiom may be tried	*/
+	register double t1,cb,t2;
+	cb = c-b;
+	if( a==c )			/* If we have only two distinct	*/
+	{				/* points linear interpolation 	*/
+	  t1 = fb/fa;			/* can only be applied		*/
+	  p = cb*t1;
+	  q = 1.0 - t1;
+ 	}
+	else				/* Quadric inverse interpolation*/
+	{
+	  q = fa/fc;  t1 = fb/fc;  t2 = fb/fa;
+	  p = t2 * ( cb*q*(q-t1) - (b-a)*(t1-1.0) );
+	  q = (q-1.0) * (t1-1.0) * (t2-1.0);
+	}
+	if( p>(double)0 )
+	{/* p was calculated with the op-*/
+	  q = -q;	
+	}/* posite sign; make p positive	*/
+	else	
+	{/* and assign possible minus to	*/
+	  p = -p;
+	}/* q				*/
+
+	if( p < (0.75*cb*q-fabs(tol_act*q)/2)	/* If b+p/q falls in [b,c]*/
+	    && p < fabs(prev_step*q/2) )
+	{/* and isn't too large	*/
+	  new_step = p/q;
+	}/* it is accepted	*/
+					/* If p/q is too large then the	*/
+					/* bissection procedure can 	*/
+					/* reduce [b,c] range to more	*/
+					/* extent			*/
+    }
+
+    if( fabs(new_step) < tol_act )
+    {/* Adjust the step to be not less*/
+      if( new_step > (double)0 )
+      {/* than tolerance		*/
+	new_step = tol_act;
+      }
+      else
+      {
+	new_step = -tol_act;
+      }
+    }
+
+    a = b;  fa = fb;			/* Save the previous approx.	*/
+    b += new_step;  fb = (*f)(b);	/* Do step to a new approxim.	*/
+    if( (fb > 0 && fc > 0) || (fb < 0 && fc < 0) )
+    {                 			/* Adjust c for it to have a sign*/
+      c = a;  fc = fa;                  /* opposite to that of b	*/
+    }
+  }
+
+}
+
+
+RNGScope scp;
+Rcpp::Function eigenfun("eigenfun");
+
 
