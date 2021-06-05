573252f09b750d5b3321e27c8c20a7b014c1ddc9
nasyring <nasyrin@gmail.com>
2021-06-05 15:42:09 -0500

    Update impred_init.c

--- impred/src/impred_init.c
+++ impred/src/impred_init.c
@@ -9,15 +9,6 @@ extern SEXP impred_randsetsMCMC(SEXP, SEXP, SEXP, SEXP, SEXP);
 extern SEXP impred_randsetspred(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
 extern SEXP impred_sigmaSolvej(SEXP, SEXP, SEXP, SEXP);
 
-
-static const R_CallMethodDef CallEntries[] = {
-    {"BayesBD_BayesBDbinary", (DL_FUNC) &BayesBD_BayesBDbinary, 8},
-    {"BayesBD_BayesBDnormal", (DL_FUNC) &BayesBD_BayesBDnormal, 9},
-    {"BayesBD_eigenfun",      (DL_FUNC) &BayesBD_eigenfun,      2},
-    {"BayesBD_unisliceL",     (DL_FUNC) &BayesBD_unisliceL,     8},
-    {NULL, NULL, 0}
-};
-
 static const R_CallMethodDef CallEntries[] = {
     {"impred_randsetsMCMC", (DL_FUNC) &impred_randsetsMCMC, 5},
     {"impred_randsetspred", (DL_FUNC) &impred_randsetspred, 8},
