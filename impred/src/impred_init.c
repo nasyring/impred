3a1dd110f4936dde5bbfa4eae61f83aa630b39ea
nasyring <nasyrin@gmail.com>
2021-06-07 15:26:07 -0500

    Update impred_init.c

--- impred/src/impred_init.c
+++ impred/src/impred_init.c
@@ -6,14 +6,14 @@
 
 /* .Call calls */
 extern SEXP impred_randsetsMCMC(SEXP, SEXP, SEXP, SEXP, SEXP);
-extern SEXP impred_randsetspred(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
+extern SEXP impred_randsetspred(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
 extern SEXP impred_sigmaSolve(SEXP, SEXP, SEXP, SEXP, SEXP);
 extern SEXP impred_zeroin(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
 extern SEXP impred_root_function(SEXP, SEXP, SEXP, SEXP, SEXP);
 
 static const R_CallMethodDef CallEntries[] = {
     {"impred_randsetsMCMC", (DL_FUNC) &impred_randsetsMCMC, 5},
-    {"impred_randsetspred", (DL_FUNC) &impred_randsetspred, 8},
+    {"impred_randsetspred", (DL_FUNC) &impred_randsetspred, 9},
     {"impred_sigmaSolve", (DL_FUNC) &impred_sigmaSolve, 5}, 
     {"impred_zeroin", (DL_FUNC) &impred_zeroin, 8},
     {"impred_root_function", (DL_FUNC) &impred_root_function, 8}, 
