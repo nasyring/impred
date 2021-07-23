92ab403de77ec9619b6426c67833a6afea68bbf6
nasyring <nasyrin@gmail.com>
2021-07-23 14:06:57 -0500

    Update Simulations.R

--- impred/R/Simulations.R
+++ impred/R/Simulations.R
@@ -4,28 +4,28 @@ source("IM.R")
 
 ### STEP 2. simulate data sets
 
-set.seed(54321)
+set.seed(54321) # for reproducibility, however, the use of c++ means we cannot perfectly reproduce runs because we cannot set the seed used in MCMC sampling inside c++
 K <- 2000
-Z.1 <- make.Z(c(13,13,13,13,13,13,13,13,13,14,1))
+Z.1 <- make.Z(c(13,13,13,13,13,13,13,13,13,14,1)) # this creates a design matrix for the model with the associated group structure.  This particular setup has 10 groups of 13 observations, plus two new observations we want to predict, one in an existing group and one in a new group
 n.1 <- sum(c(13,13,13,13,13,13,13,13,13,14,1))
-n <- n.1-2
-Z <- Z.1[1:n,1:(ncol(Z.1)-1)]
-sigma_a2 <- 0.1
-sigma_2 <- 2
-mu <- 6
-Y.mat <- matrix(0,n.1,K)
+n <- n.1-2  # 130 total data points, and two unobserved points we will predict
+Z <- Z.1[1:n,1:(ncol(Z.1)-1)]	# the design matrix taking out the new observations
+sigma_a2 <- 0.1	# within group standard deviation
+sigma_2 <- 2	# across group standard deviation
+mu <- 6	# mean parameter
+Y.mat <- matrix(0,n.1,K)	# generating matrix of data plus two new observations
 for(i in 1:K){
 	Y.mat[,i] <- sample.Y(Z.1, sigma_a2, sigma_2, mu)
 }
-Y <- Y.mat[1:n,]
-Y.new <- t(Y.mat[(n+1):n.1,])
+Y <- Y.mat[1:n,]	# keeping just the n observations
+Y.new <- t(Y.mat[(n+1):n.1,])	# picking out just the 2 new obs for prediction
 
 
 ### STEP 3. compute fused plausibility curves for each data set
 
-plaus.results <- matrix(0,500,2*K)
+plaus.results <- matrix(0,500,2*K)	# we have a total of K data sets of size n each along with 2 new obs to predict for each of the K data sets
 
-
+# looping through all K sets and computing IM predictive plausibility curves for our two predictions: one within group and one new group
 for(k in 1:K){
 	time <- proc.time()
 	ex.data <- list(Y=Y[,k], Z=Z)
@@ -42,10 +42,11 @@ for(k in 1:K){
 
 ### STEP 4. check coverage of prediction intervals
 
+# looping through all K data sets and computing prediction intervals at 95%, 90%, and 80% from our IMs for our 2 predictions each
 
 coverage<-matrix(0,K,6)
 length<-coverage
-for(k in 1907:K){
+for(k in 1:K){
 	# within prediction
 	plaus.max <- which.max(plaus.results[,(2*k-1)])
 	lower.plaus <- which.min(abs(.05-plaus.results[1:plaus.max,(2*k-1)]))
@@ -83,15 +84,8 @@ for(k in 1907:K){
 	length[k,6] <- pi.80[2]-pi.80[1]
 }
 
-#sig.grid = as.matrix(expand.grid(seq(from = 0.01, to = 7, length.out = 14), seq(from = 0.01, to = 7, length.out = 14)))
-> colMeans(coverage)
-[1] 0.9325 0.8685 0.7585 0.9255 0.8670 0.7540
-> colMeans(length)
-[1] 7.443020 6.201059 4.822647 7.482341 6.231320 4.846368
 
-
-
-#### STEP 5: bootstrap pred - needs updating
+#### STEP 5: Compare to bootstrap - using Fernando's bootstrap function in predintma
 
 group.des <- c(13,13,13,13,13,13,13,13,13,13)
 k <- length(group.des)
@@ -99,26 +93,30 @@ group <- c()
 for(j in 1:k){
 	group <- c(group, rep(j,group.des[j])) 
 }
-coverage.boot <- matrix(NA, K,3)
-length.boot <- matrix(NA, K,3)
+coverage.boot.w <- matrix(NA, K,3)
+length.boot.w <- matrix(NA, K,3)
+coverage.boot.n <- matrix(NA, K,3)
+length.boot.n <- matrix(NA, K,3)
 
-for(i in 510:K){
+
+# first set of bootstrap prediction intervals is for the new observation in an existing group
+for(i in 1:K){
 	response <- c(Y[,i],Y.new[i,1])
 	my.data = data.frame(response, group)
 	pdi.80 <- pred_int_boot(formula = response ~ 1 + (1|group), data = my.data, level = 0.8, R = 5000)
 	pdi.90 <- pred_int_boot(formula = response ~ 1 + (1|group), data = my.data, level = 0.9, R = 5000)
 	pdi.95 <- pred_int_boot(formula = response ~ 1 + (1|group), data = my.data, level = 0.95, R = 5000)
-	coverage.boot[i,3] <- ifelse((pdi.80[2]<=Y.new[i,2]) & (pdi.80[3]>=Y.new[i,2]), 1, 0)
-	length.boot[i,3] <- pdi.80[3]-pdi.80[2]
-	coverage.boot[i,2] <- ifelse((pdi.90[2]<=Y.new[i,2]) & (pdi.90[3]>=Y.new[i,2]), 1, 0)
-	length.boot[i,2] <- pdi.90[3]-pdi.90[2]
-	coverage.boot[i,1] <- ifelse((pdi.95[2]<=Y.new[i,2]) & (pdi.95[3]>=Y.new[i,2]), 1, 0)
-	length.boot[i,1] <- pdi.95[3]-pdi.95[2]
+	coverage.boot.w[i,3] <- ifelse((pdi.80[2]<=Y.new[i,2]) & (pdi.80[3]>=Y.new[i,2]), 1, 0)
+	length.boot.w[i,3] <- pdi.80[3]-pdi.80[2]
+	coverage.boot.w[i,2] <- ifelse((pdi.90[2]<=Y.new[i,2]) & (pdi.90[3]>=Y.new[i,2]), 1, 0)
+	length.boot.w[i,2] <- pdi.90[3]-pdi.90[2]
+	coverage.boot.w[i,1] <- ifelse((pdi.95[2]<=Y.new[i,2]) & (pdi.95[3]>=Y.new[i,2]), 1, 0)
+	length.boot.w[i,1] <- pdi.95[3]-pdi.95[2]
 	if(i%%100 == 0) print(i)
 }
 
-colMeans(coverage.boot, na.rm = TRUE)
-colMeans(length.boot)
+colMeans(coverage.boot.w, na.rm = TRUE)
+colMeans(length.boot.w)
 
 > colMeans(coverage.boot[1:500,])
 [1] 0.552 0.484 0.372
@@ -130,3 +128,23 @@ colMeans(length.boot)
 > colMeans(length.boot, na.rm = TRUE)
 [1] 2.926005 2.473079 1.934625
 
+
+
+# second set of bootstrap prediction intervals is for the new observation in an existing group
+for(i in 1:K){
+	response <- c(Y[,i],Y.new[i,2])
+	my.data = data.frame(response, group)
+	pdi.80 <- pred_int_boot(formula = response ~ 1 + (1|group), data = my.data, level = 0.8, R = 5000)
+	pdi.90 <- pred_int_boot(formula = response ~ 1 + (1|group), data = my.data, level = 0.9, R = 5000)
+	pdi.95 <- pred_int_boot(formula = response ~ 1 + (1|group), data = my.data, level = 0.95, R = 5000)
+	coverage.boot.n[i,3] <- ifelse((pdi.80[2]<=Y.new[i,2]) & (pdi.80[3]>=Y.new[i,2]), 1, 0)
+	length.boot.n[i,3] <- pdi.80[3]-pdi.80[2]
+	coverage.boot.n[i,2] <- ifelse((pdi.90[2]<=Y.new[i,2]) & (pdi.90[3]>=Y.new[i,2]), 1, 0)
+	length.boot.n[i,2] <- pdi.90[3]-pdi.90[2]
+	coverage.boot.n[i,1] <- ifelse((pdi.95[2]<=Y.new[i,2]) & (pdi.95[3]>=Y.new[i,2]), 1, 0)
+	length.boot.n[i,1] <- pdi.95[3]-pdi.95[2]
+	if(i%%100 == 0) print(i)
+}
+
+colMeans(coverage.boot.n, na.rm = TRUE)
+colMeans(length.boot.n)
