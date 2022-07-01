1bd219aaedb3cd0d28eba62c7edbf86ba90fd664
nasyring <nasyrin@gmail.com>
2022-07-01 13:09:05 -0500

    Update impred.cpp

--- impred/src/impred.cpp
+++ impred/src/impred.cpp
@@ -164,12 +164,42 @@ result = Rcpp::List::create(Rcpp::Named("randsetpred") = randsetpred);
 }
 
 
-Rcpp::List randsetspredlmer() {
+Rcpp::List randsetspredlmer(NumericMatrix S, NumericVector dimS, NumericVector U, NumericMatrix C1, NumericMatrix C2, NumericVector By, NumericVector x, NumericVector ztz) {
 	
 	List result;
 	
+	int M = int(dimS[0]);
+	arma::mat C1a = as<arma::mat>(C1);
+	arma::mat C2a = as<arma::mat>(C2);
+	arma::colvec xa = as<arma::colvec>(x);
+	arma::colvec Bya = as<arma::colvec>(By);
+	arma::mat xBy = xa.t()*Bya;
+	arma::mat Csigma; arma::mat total_sigma;
+	NumericVector Uu(1, 0.0); NumericVector Ul(1, 0.0);
+	NumericVector zeroes(20000,0.0);
+	NumericMatrix randsetpred = NumericMatrix(10000, 2, zeroes.begin());
+	NumericVector thetas(10000, 0.0);
+		
+	for(int j=0; j < 10000; j++){
+		Z[0] = R::rnorm(0.0,1.0);
+		Csigma = S(j,1)*C1a + S(j,0)*C2a;
+		total_sigma = xa.t()*Csigma*xa + S(j,0)*ztz;
+		total_sigma(0,0) = std::sqrt(total_sigma(0,0));
+		thetas[j] = Z[0]*total_sigma(0,0) + xBy(0,0);
+	}
+
+
+	std::sort(thetas.begin(), thetas.end());
+
 	
-	result = Rcpp::List::create(Rcpp::Named("") = );
+	for(int j=0; j < 10000; j++){
+		Ul[0] = 0.5-std::fabs(U[j]-0.5);
+		Uu[0] = 1.0-Ul[0];
+		randsetpred(j,0) = thetas[std::round(10000*Ul[0])];
+		randsetpred(j,1) = thetas[std::round(10000*Uu[0])];
+	}
+	
+	result = Rcpp::List::create(Rcpp::Named("randsetpred") = randsetpred);
 
 	return result;
 	
