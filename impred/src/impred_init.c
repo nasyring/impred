f6139f73e93447ddbc3a3d246531fb54102dc74f
nasyring <nasyrin@gmail.com>
2022-07-01 13:10:01 -0500

    Update impred_init.c

--- impred/src/impred_init.c
+++ impred/src/impred_init.c
@@ -7,6 +7,7 @@
 /* .Call calls */
 extern SEXP impred_randsetsMCMC(SEXP, SEXP, SEXP, SEXP, SEXP);
 extern SEXP impred_randsetspred(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
+extern SEXP impred_randsetspredlmer(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
 extern SEXP impred_genIM(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
 extern SEXP impred_randsetspreddens(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
 extern SEXP impred_sigmaSolve(SEXP, SEXP, SEXP, SEXP, SEXP);
@@ -14,11 +15,10 @@ extern SEXP impred_zeroin(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
 extern SEXP impred_root_function(SEXP, SEXP, SEXP, SEXP, SEXP);
 
 
-
-
 static const R_CallMethodDef CallEntries[] = {
     {"impred_randsetsMCMC", (DL_FUNC) &impred_randsetsMCMC, 5},
     {"impred_randsetspred", (DL_FUNC) &impred_randsetspred, 8},
+    {"impred_randsetspredlmer", (DL_FUNC) &impred_randsetspred, 8},
     {"impred_genIM", (DL_FUNC) &impred_genIM, 6},
     {"impred_randsetspreddens", (DL_FUNC) &impred_randsetspreddens, 11},
     {"impred_sigmaSolve", (DL_FUNC) &impred_sigmaSolve, 5}, 
