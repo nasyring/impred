9e232e92fdee4a7dec78b4df3ea3050b08936de3
nasyring <nasyrin@gmail.com>
2022-07-19 10:16:24 -0500

    Update RcppExports.cpp

--- impred/src/RcppExports.cpp
+++ impred/src/RcppExports.cpp
@@ -22,6 +22,25 @@ BEGIN_RCPP
 END_RCPP
 }
 
+
+plaus_balanced_aov(NumericVector theta, NumericVector Ybar, NumericVector S, NumericVector lambda, NumericVector r, NumericVector n, NumericVector n_i);
+RcppExport SEXP impred_plaus_balanced_aov(SEXP thetaSEXP, SEXP YbarSEXP, SEXP SSEXP, SEXP lambdaSEXP, SEXP rSEXP, SEXP nSEXP, SEXP n_iSEXP){
+BEGIN_RCPP
+    Rcpp::RObject __result;
+    Rcpp::RNGScope __rngScope;
+    Rcpp::traits::input_parameter< NumericVector >::type theta(thetaSEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type Ybar(YbarSEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type S(SSEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type lambda(lambdaSEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type r(rSEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type n(nSEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type n_i(n_iSEXP);
+    __result = Rcpp::wrap(plaus_balanced_aov(theta, Ybar, S, lambda, r, n, n_i));
+    return __result;
+END_RCPP
+}
+
+
 Rcpp::List plaus_balanced(NumericVector thetaseq, NumericVector saseq, NumericVector seseq, NumericVector n, NumericVector n_i, NumericVector S, NumericVector lambda, NumericVector r, NumericVector Ybar, NumericVector numk);
 RcppExport SEXP impred_plaus_balanced(SEXP thetaseqSEXP, SEXP saseqSEXP, SEXP seseqSEXP, SEXP nSEXP, SEXP n_iSEXP, SEXP SSEXP, SEXP lambdaSEXP, SEXP rSEXP, SEXP YbarSEXP, SEXP numkSEXP){
 BEGIN_RCPP
