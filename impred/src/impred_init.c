6a3398f39fd728624869255fca23103afddb0351
nasyring <nasyrin@gmail.com>
2022-07-25 14:35:09 -0500

    Update impred_init.c

--- impred/src/impred_init.c
+++ impred/src/impred_init.c
@@ -21,7 +21,6 @@ extern SEXP impred_plaus_marginal_lmer(SEXP, SEXP, SEXP);
 extern SEXP impred_plaus_balanced_aov(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
 extern SEXP impred_plaus_unbalanced_aov(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
 extern SEXP impred_plaus_two_stage(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
-extern SEXP impred_auxiliaryMCMC(SEXP, SEXP, SEXP, SEXP);
 
 static const R_CallMethodDef CallEntries[] = {
     {"impred_randsetsMCMC", (DL_FUNC) &impred_randsetsMCMC, 5},
@@ -40,7 +39,6 @@ static const R_CallMethodDef CallEntries[] = {
   {"impred_plaus_balanced_aov", (DL_FUNC) &impred_plaus_balanced_aov, 7},
   {"impred_plaus_unbalanced_aov", (DL_FUNC) &impred_plaus_unbalanced_aov, 9},
   {"impred_plaus_two_stage", (DL_FUNC) &impred_plaus_two_stage, 8},
-    {"impred_auxiliaryMCMC", (DL_FUNC) &impred_auxiliaryMCMC, 4},
     {NULL, NULL, 0}
 };
 
@@ -49,4 +47,3 @@ void R_init_impred(DllInfo *dll)
     R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
     R_useDynamicSymbols(dll, FALSE);
 }
-
