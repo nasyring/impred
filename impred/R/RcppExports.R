bb8cfc8d27a961b7554d58a5f18470d28c0bb0a7
nasyring <nasyrin@gmail.com>
2022-09-18 16:31:42 -0500

    Update RcppExports.R

--- impred/R/RcppExports.R
+++ impred/R/RcppExports.R
@@ -2,8 +2,8 @@
 # Generator token: 10BE3573-1514-4C36-9D1C-5A225CD40393
 
 
-IMTS_mh_sampler2 <- function(lU0, V0, H0, Minv, rL, thetau0s, propsd1, propsd2, sampsize) {
-    .Call(`impred_IMTS_mh_sampler2`, lU0, V0, H0, Minv, rL, thetau0s, propsd1, propsd2, sampsize)
+IMTS_mh_sampler2 <- function(lU0, V0, H0, Minv, rL, thetau0s, y0s, propsd1, propsd2, sampsize) {
+    .Call(`impred_IMTS_mh_sampler2`, lU0, V0, H0, Minv, rL, thetau0s, y0s, propsd1, propsd2, sampsize)
 }
 
 IMTS_mh_sampler <- function(lU0, V0, H0, Minv, rL, thetau0s, propsd1, propsd2, sampsize) {
